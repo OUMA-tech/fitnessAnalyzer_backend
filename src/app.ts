c06f7bfe3e19f73107433284122cfc085b92e0c3
OUMA-tech <tianyfff11@gmail.com>
2025-06-10 23:41:40 +1000

    changed cors to www.fit-tracker.fyi

--- src/app.ts
+++ src/app.ts
@@ -63,7 +63,7 @@ const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://www.fit-tracker.fyi',
   credentials: true
 }));
 app.use(bodyParser.json());
